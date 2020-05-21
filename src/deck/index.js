0178664651142218dda2c55eac978f380a43e283
jianyuan.gjy <jianyuan.gjy@dtwave-inc.com>
2020-05-21 16:47:14 +0800

    Initial commit

--- src/deck/index.js
+++ src/deck/index.js
@@ -1,91 +1,186 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, createRef} from 'react'
 import DeckGL from '@deck.gl/react'
 import {StaticMap} from 'react-map-gl'
-import {CesiumIonLoader, Tiles3DLoader} from '@loaders.gl/3d-tiles'
+import {Tiles3DLoader} from '@loaders.gl/3d-tiles'
 import {Tile3DLayer} from '@deck.gl/geo-layers'
+import {GeoJsonLayer, TextLayer} from '@deck.gl/layers'
+import {Matrix4} from 'math.gl'
+import {FlyToInterpolator} from 'deck.gl'
+import {LightingEffect, _SunLight as SunLight, PointLight} from '@deck.gl/core'
+
+import {debounce} from 'lodash'
 import mapboxStyle from './mapboxStyle'
+import geoJson from './geojson'
+import polygon from './polygon'
+import nameGeo from './name'
+
 
 window.oncontextmenu = function (e) {
   // 取消默认的浏览器自带右键 很重要！！
   e.preventDefault() 
 }
 
+
 const Deck = () => {
   const [layers, setLayers] = useState([])
-  useEffect(() => {
-    // effect
-    // return () => {
-    //   cleanup
-    // // }
-    // const deckInstance = new DeckGL({
-    //   mapboxApiAccessToken: 'pk.eyJ1IjoibHZvbiIsImEiOiJjazIyb2gwczEwbjhrM2NuMDJwdWtoZG1wIn0.ry6dA_3yn1iAstmqMABGKw',
-    //   mapStyle: style,
-    //   initialViewState: {
-    //     longitude: 118.988491,
-    //     latitude: 32.139774,
-    //     zoom: 12
-    //   },
-    //   controller: true,
-    //   container: 'map',
-    //   layers: [
-    //     new deck.Tile3DLayer({
-    //       id: 'tile-3d-layer',
-    //       // tileset json file url 
-    //       data: 'http://hxxixia.lcfw.co:7119/nanjingmapdata/realModel/TYY-3DTiles2/Scene/TYYm3DTiles2.json',
-    //       loader: deck.CesiumIonLoader,
-    //     })
-    //   ]
-    // });
-    console.log('CesiumIonLoader:', CesiumIonLoader)
-    setLayers([
-      new Tile3DLayer({
-        id: 'tile-3d-layer',
-        // tileset json file url 
-        data: 'https://cdn.ilvon.cn/lvcheng/nanjing/whiteModel/tianyouyuan/tileset.json',
-        loader: Tiles3DLoader,
-        onTilesetLoad: tileHeader => {
-          console.log('load', tileHeader)
-        },
-        opacity: 0.5,
-        modelMatrix: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      }),
+  const deck = createRef()
+  window.deck = deck
 
-      new Tile3DLayer({
-        id: 'real-3d-layer',
-        // tileset json file url 
-        data: 'https://smartc.wasu.cn/3dtile/tileset.json',
-        loader: Tiles3DLoader,
-        loadOptions: {
-          tileset: {
-            type: 'scenegraph',
-          },
-        },
-        onTilesetLoad: () => {
-          console.log('load')
-        },
-        onTileError: (tileHeader, url, message) => {
-          console.log(url, message)
-        },
-      }),
+  const initViewState = {
+    longitude: 120.16735957533727,
+    latitude: 30.165954724984655,
+    zoom: 18,
+    pitch: 0,
+    bearing: 30,
+    maxPitch: 90,
+    transitionDuration: 3000,
+    transitionInterpolator: new FlyToInterpolator(),
+  }
+  const [xy, setXY] = useState([0, 0])
+
+  const [viewState, setViewState] = useState(initViewState)
+
+
+  const flyTo = xyz => {
+    setViewState({
+      ...viewState,
+      ...xyz,
+    })
+  }
+
+  useEffect(() => {
+    // const debounceSetXY = debounce(() => {
+    if (deck?.current?.viewports) {
+      console.log('11111')
+      const [x, y] = deck.current.viewports[0].project([120.16902492151641, 30.167722553749094, 57.1])
+      setXY([x, y])
+    }
+    // }, 100)
+    // debounceSetXY()
+  },
+  [viewState])
+
+  // window.flyTo = flyTo
+  // window.setViewState = setViewState
+  useEffect(() => {
+    // 倾斜摄影模型 火炬小区
+    const real3Dlayer = new Tile3DLayer({
+      id: 'real-3d-layer',
+      data: 'https://smartc.wasu.cn/3dtile/tileset.json',
+      loader: Tiles3DLoader,
+      onTileLoad: tileHeader => {
+        // console.log('3d tileHeader',tileHeader)
+        const x = -90 / 180 * Math.PI
+        tileHeader.content.modelMatrix = new Matrix4(tileHeader.content.modelMatrix).rotateX(x).translate([0, 0, -19])
+        return tileHeader
+      },
+    })
+
+
+    // 火炬小区楼栋名称
+    const nameLayer = new TextLayer({
+      id: 'geojson-name',
+      data: 'https://yfcctv.wasu.cn/shidian-mapdata/area/village/huojvxiaoqu-label.geojson',
+      dataTransform: data => data.features,
+      getPosition: d => [...d.geometry.coordinates, d.properties.height],
+      getText: d => d.properties.name,
+      getSize: 32,
+      getAngle: 0,
+      getTextAnchor: 'start',
+      getAlignmentBaseline: 'center',
+      getTransformMatrix: d => {
+        console.log(d)
+      },
+      backgroundColor: [255, 255, 0],
+      pickable: true,
+      getColor: [255, 0, 0, 255],
+      onHover: (a, b) => {
+        console.log('a:', a)
+        console.log('b:', b)
+      },
+    })
+
+    window.nameLayer = nameLayer
+
+
+    // 火炬小区拉高模型
+    const polygonLayer = new GeoJsonLayer({
+      id: 'geojson-click',
+      data: 'https://yfcctv.wasu.cn/shidian-mapdata/area/building/huojvxiaoqu.geojson',
+      pickable: true,
+      filled: true,
+      extruded: true,
+      opacity: 0,
+      getElevation: obj => obj.properties.height,
+      onClick: (obj, event) => {
+        console.log(obj.object.properties.name)
+        console.log(event)
+        return true
+      },
+    })
+
+    // 火炬小区边界范围
+    const shapeLayer = new GeoJsonLayer({
+      id: 'geojson-layer',
+      data: 'https://yfcctv.wasu.cn/shidian-mapdata/area/village/huojvxiaoqu.geojson',
+      pickable: true,
+      stroked: true,
+      filled: false,
+      getLineColor: [0, 0, 255, 100],
+      getLineWidth: 5,
+      onHover: ({object, x, y}) => {
+        if (object) {
+          if (document.getElementById('test')) {
+            const div = document.getElementById('test')
+            div.style.top = `${y}px`
+            div.style.left = `${x}px`
+          } else {
+            const div = document.createElement('div')
+            div.id = 'test'
+            div.style.position = 'fixed'
+            div.style.top = `${y}px`
+            div.style.left = `${x}px`
+            div.style.padding = '8px'
+            div.style.background = 'rbga(255,255,255,0.4)'
+            div.innerHTML = object.properties.name
+
+            document.body.appendChild(div)
+          }
+        } else {
+          document.getElementById('test') && document.getElementById('test').remove()
+        }
+      },
+    })
+
+    setLayers([
+      nameLayer,
+      shapeLayer,
+      real3Dlayer,
+      polygonLayer,
     ])
   }, [])
+
+
   return <div
     id="map"
     style={{
+      transform: 'scale(0.85)',
       height: '90vh',
       width: '90vw',
       margin: '40px',
     }}>
+
+    <button 
+      onClick={() => {
+        flyTo(initViewState)
+      }}>
+      test
+    </button>
+
+    <div>{JSON.stringify(viewState)}</div>
     <DeckGL
-      initialViewState={{
-        // longitude: 118.988491,
-        // latitude: 32.139774,
-        longitude: 120,
-        latitude: 30,
-        zoom: 12,
-        pitch: 60,
-        bearing: 0,
-      }}
+      ref={deck}
+      viewState={viewState}
       style={{
         height: '100%',
         width: '100%',
@@ -93,6 +188,39 @@ const Deck = () => {
       }}
       controller
       layers={layers}
+      // effects={[
+      //   new LightingEffect({
+      //     sunLight: new SunLight({
+      //       timestamp: new Date('2020-05-21 12:00').getTime(), 
+      //       color: [255, 255, 255],
+      //       intensity: 1,
+      //     }),
+      //     pointerLight: new PointLight({
+      //       color: [128, 128, 0],
+      //       intensity: 0.02,
+      //       position: [120.16902492151641, 30.167722553749094, 200],
+      //       //         longitude: 120.16735957533727,
+      //       // latitude: 30.165954724984655,
+      //     }),
+      //   }),
+        
+      // ]}
+      // onClick={(info, event) => {
+      //   console.log(info, event)
+      // }}
+      // getTooltip={info => {
+      //   // console.log(info)
+      //   const rs = {
+      //     text: `${JSON.stringify(info.coordinate)}<br/>${JSON.stringify(info.pixel)}`,
+      //   }
+      //   return rs
+      // }}
+
+      onViewStateChange={state => {
+        // console.log(state)
+        setViewState(state.viewState)
+      }}
+
     >
       <StaticMap
         draggable
@@ -101,7 +229,17 @@ const Deck = () => {
       />
 
     </DeckGL>
-
+    <div
+      style={{
+        position: 'fixed',
+        left: xy[0],
+        top: xy[1],
+        height: '10px',
+        width: '10px',
+        background: 'red',
+        zIndex: 10,
+      }}
+    >{JSON.stringify(xy)}</div>
   </div>
 }
 export default Deck
---
+++ src/deck/name.js
@@ -0,0 +1,618 @@
+export default {
+  type: 'FeatureCollection',
+  name: 'building',
+  features: [
+    {
+      type: 'Feature',
+      properties: {
+        id: '0EA3238A-7DBB-4ACD-8FA6-4E18BD0F7A73',
+        community: '火炬小区',
+        height: 38.5,
+        label: 1,
+        name: '86幢',
+        ORIG_FID: 0,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16587808575802,
+          30.165472443800425,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '79142173-C42C-410D-AEC2-F750A5FB7C24',
+        community: '火炬小区',
+        height: 38.5,
+        label: 1,
+        name: '87幢',
+        ORIG_FID: 1,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16618286324224,
+          30.165586015042845,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '1EA61F1B-88B0-4222-996A-59A08BB8300B',
+        community: '火炬小区',
+        height: 38.5,
+        label: 1,
+        name: '88幢',
+        ORIG_FID: 2,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16648714024831,
+          30.165699178701015,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'A7313663-CC24-45EC-8F31-D3555F5A0A92',
+        community: '火炬小区',
+        height: 38.5,
+        label: 1,
+        name: '89幢',
+        ORIG_FID: 3,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16679013703062,
+          30.16581158365921,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'E6611E73-F89E-4789-8DA9-9C6CE97444DB',
+        community: '火炬小区',
+        height: 37.7,
+        label: 1,
+        name: '85幢',
+        ORIG_FID: 4,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16624882178691,
+          30.16524871505674,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'E7D844E4-4CC4-4DF2-91AE-A73F519496EB',
+        community: '火炬小区',
+        height: 37.7,
+        label: 1,
+        name: '84幢',
+        ORIG_FID: 5,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16675324443332,
+          30.165425493664916,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'BD580F58-8621-4AE3-9953-B97E4DC276F1',
+        community: '火炬小区',
+        height: 37.7,
+        label: 1,
+        name: '83幢',
+        ORIG_FID: 6,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16725958648983,
+          30.16561417140168,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '81DA1484-63AA-468F-816F-31FA6D032E8F',
+        community: '火炬小区',
+        height: 37.7,
+        label: 1,
+        name: '82幢',
+        ORIG_FID: 7,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16773991712986,
+          30.165784324119716,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '84802069-1FEB-4FEA-89C2-723296AABC89',
+        community: '火炬小区',
+        height: 57,
+        label: 1,
+        name: '94幢',
+        ORIG_FID: 8,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16564260181258,
+          30.16578676145766,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '11832193-E089-4B45-B419-60FA2E948378',
+        community: '火炬小区',
+        height: 56.9,
+        label: 1,
+        name: '93幢',
+        ORIG_FID: 9,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16587148262957,
+          30.166225141620895,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'EA6AF9FF-3C13-4F4C-ADA4-2B0412BCC3D9',
+        community: '火炬小区',
+        height: 56.7,
+        label: 1,
+        name: '92幢',
+        ORIG_FID: 10,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16608712612705,
+          30.16666356064717,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'E9444561-A4AF-437E-B086-A3E20B0AEE8D',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '91幢',
+        ORIG_FID: 11,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16636917348835,
+          30.16639874296779,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '1EBD61DD-D5C2-454F-BFD1-843921EAEEE8',
+        community: '火炬小区',
+        height: 36.3,
+        label: 1,
+        name: '90幢',
+        ORIG_FID: 12,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16635837249662,
+          30.166090803191466,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '7CE4A271-4D95-4AF0-8F89-CED329EAA74E',
+        community: '火炬小区',
+        height: 38.6,
+        label: 1,
+        name: '80幢',
+        ORIG_FID: 13,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.1669472097649,
+          30.16633318684946,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '2EFFCC0B-AAFE-4FAA-89C1-87050CB4857F',
+        community: '火炬小区',
+        height: 30.4,
+        label: 1,
+        name: '幼儿园',
+        ORIG_FID: 14,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16785696872832,
+          30.166390237857378,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '3C1258CC-68F9-40B0-8B32-D5850D1694D7',
+        community: '火炬小区',
+        height: 38.1,
+        label: 1,
+        name: '11幢',
+        ORIG_FID: 15,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16758310174916,
+          30.166641739958724,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '9FFB7D34-7738-4ED5-851A-4A34B7D499B7',
+        community: '火炬小区',
+        height: 39,
+        label: 1,
+        name: '12幢',
+        ORIG_FID: 16,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16752445398113,
+          30.16697548961441,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '9DBE6B0B-71D8-4F9F-869D-E8356AA8607B',
+        community: '火炬小区',
+        height: 38.1,
+        label: 1,
+        name: '79幢',
+        ORIG_FID: 17,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16691860712255,
+          30.166653852851873,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '0EC33801-DBF9-4BED-AC32-5340BB83E477',
+        community: '火炬小区',
+        height: 38,
+        label: 1,
+        name: '78幢',
+        ORIG_FID: 18,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16656036901396,
+          30.16684738045913,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '9683DACE-05BE-44CB-A606-FDD4C2D242BB',
+        community: '火炬小区',
+        height: 38,
+        label: 1,
+        name: '77幢',
+        ORIG_FID: 19,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16701206013309,
+          30.167021239728818,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '1F07E29F-71A8-44C3-B60B-D9D8D63A3674',
+        community: '火炬小区',
+        height: 73.1,
+        label: 1,
+        name: '13幢',
+        ORIG_FID: 20,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.1674901402267,
+          30.16733581208446,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '1422A19F-C670-4376-AC58-4D2936421049',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '61幢',
+        ORIG_FID: 21,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16867334160898,
+          30.16634655990503,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '794421D9-8A95-486A-A2CD-15E99DB39B29',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '62幢',
+        ORIG_FID: 22,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16907484785848,
+          30.166378956276382,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'C32F5932-F66E-4C62-8E35-563B1A9263BB',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '68幢',
+        ORIG_FID: 23,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16874730532972,
+          30.166701320758726,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '1EAC8FCC-6494-4F74-A325-13B48B180716',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '69幢',
+        ORIG_FID: 24,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16842302305787,
+          30.166735905305515,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'B7656471-A238-4BFD-8E6D-FFD4182CE2EC',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '66幢',
+        ORIG_FID: 25,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16902390136573,
+          30.16701541331185,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '6263B7BC-0726-4EC8-A066-9625E087687A',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '70幢',
+        ORIG_FID: 26,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16827045579602,
+          30.1670360253698,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'B234C5C6-907B-49FA-BEA5-7D5BF1FE30EB',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '71幢',
+        ORIG_FID: 27,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16879202916198,
+          30.167234754285825,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '25615053-6733-4B99-9718-4EC9A7F2FC25',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '72幢',
+        ORIG_FID: 28,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16919579860668,
+          30.167436622865516,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'D1E41F26-58BC-40B6-A5E0-C26B5A71BCC7',
+        community: '火炬小区',
+        height: 57.1,
+        label: 1,
+        name: '75幢',
+        ORIG_FID: 29,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16902492151641,
+          30.167722553749094,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '77785B34-0326-4200-A14A-61763E627117',
+        community: '火炬小区',
+        height: 57.1,
+        label: 1,
+        name: '76幢',
+        ORIG_FID: 30,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16838786132311,
+          30.16747707872485,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'FF54FCF0-9E94-4FBD-9C45-DCEB71A216DB',
+        community: '火炬小区',
+        height: 38.1,
+        label: 1,
+        name: '10幢',
+        ORIG_FID: 31,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16788723324746,
+          30.166760746087906,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'F0B23978-3F81-4F3F-9893-B36CFB54C2EB',
+        community: '火炬小区',
+        height: 37.9,
+        label: 1,
+        name: '67幢',
+        ORIG_FID: 32,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16911096484316,
+          30.166717467878897,
+        ],
+      },
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: 'EBF734AF-137E-4D4A-A3DB-F0FAAD0E6833',
+        community: '火炬小区',
+        height: 38.1,
+        label: 1,
+        name: '95幢',
+        ORIG_FID: 33,
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          120.16557680138796,
+          30.16660207023865,
+        ],
+      },
+    },
+  ],
+}
